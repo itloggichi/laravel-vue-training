7e59b00ac542a894d26c04da9964bd48784b4105
itloggichi <kcalmendrala@up.edu.ph>
2022-07-07 21:36:49 +0800

    [feat]: Added basket feature and created initial markup of Checkout page

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,29 +1,35 @@
 import VueRouter from "vue-router";
-import Bookables from "./bookables/Bookables";
+import Basket from "./basket/Basket";
 import Bookable from "./bookable/Bookable";
+import Bookables from "./bookables/Bookables";
 import Review from "./review/Review";
 
 const routes = [
     {
         path: "/",
         component: Bookables,
-        name: "home",
+        name: "home"
     },
     {
         path: "/bookable/:id",
         component: Bookable,
-        name: "bookable",
+        name: "bookable"
     },
     {
         path: "/review/:id",
         component: Review,
         name: "review"
+    },
+    {
+        path: "/basket",
+        component: Basket,
+        name: "basket"
     }
 ];
 
 const router = new VueRouter({
-    routes,
-    mode: "history" 
+    routes, // short for `routes: routes`
+    mode: "history"
 });
 
 export default router;

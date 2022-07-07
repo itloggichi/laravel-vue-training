5f6e2a975ed62347b5c69ec297210e6b3bcb3691
itloggichi <kcalmendrala@up.edu.ph>
2022-07-07 22:02:02 +0800

    [feat]: Added an API endpoint for checkout validation form

--- app/Booking.php
+++ app/Booking.php
@@ -8,15 +8,21 @@ use Illuminate\Support\Str;
 
 class Booking extends Model
 {
-    protected $fillable = ['from', 'to']; //for security reasons
-    
-    public function bookable() {
+    protected $fillable = ['from', 'to'];
+
+    public function bookable()
+    {
         return $this->belongsTo(Bookable::class);
     }
 
     public function review()
     {
         return $this->hasOne(Review::class);
+    }
+
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
     }
 
     public function scopeBetweenDates(Builder $query, $from, $to)
@@ -30,7 +36,8 @@ class Booking extends Model
         return static::where('review_key', $reviewKey)->with('bookable')->get()->first();
     }
 
-    protected static function boot() {
+    protected static function boot()
+    {
         parent::boot();
 
         static::creating(function ($booking) {
